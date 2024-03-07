85bac84ef6d234177950081f12ee573f0f388f76
Javier <javier.casas@3xmgroup.com>
2024-03-07 13:24:16 -0300

    Add Routing and custom Bootstrap

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { AccountService } from '../_services/account.service';
 import { User } from '../_modules/user';
 import { Observable, of } from 'rxjs';
+import { Router } from '@angular/router';
+import { Toast, ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-nav',
@@ -12,22 +14,21 @@ export class NavComponent {
   model: any = {};
   userData: User | null = localStorage.getItem('user') ? JSON.parse(localStorage.getItem('user') || '{}') : null;
 
-  constructor(public accountService: AccountService) { }
+  constructor(public accountService: AccountService, private router: Router, private toastr: ToastrService) { }
 
   ngOnInit() {
   }
-  
+
   login() {
     this.accountService.login(this.model).subscribe({
-      next: response => { 
-      console.log(response)
-    }, 
-    error: error => console.log(error)
+      next: _ => this.router.navigateByUrl('/members'),
+      error: error => this.toastr.error("Invalid username or password. Please try again.")
     })
   }
 
   logout() {
     this.accountService.logout();
+    this.router.navigateByUrl('/');
   }
 
 }
